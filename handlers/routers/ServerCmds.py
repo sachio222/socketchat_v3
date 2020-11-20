19fa4165d8da978cecf0d6c45a6af179a6478a97
sachio222 <brainpowerux@gmail.com>
2020-11-19 22:31:34 -0500

    bugfix

--- handlers/routers/ServerCmds.py
+++ handlers/routers/ServerCmds.py
@@ -31,7 +31,7 @@ def _b_handler(sock: socket, buffer: dict, *args, **kwargs):
                                 prefixes.dict["server"]["chat"]["sysMsg"],
                                 sysMsgList.bootSelf)
                 else:
-                    Booter is not self.
+                    # Booter is not self.
                     ChatIO().pack_n_send(sock,
                                 prefixes.dict["server"]["chat"]["sysMsg"],
                                 f"{sysMsgList.bootSuccess} {bootee}")
